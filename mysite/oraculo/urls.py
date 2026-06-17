28836e939db230c1be2407880f977a026e6f2ba3
Jonathan Duarte <devjonathanduarte@gmail.com>
2026-06-17 02:10:38 -0300

    nova url adicionada

--- mysite/oraculo/urls.py
+++ mysite/oraculo/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("stream_response/", views.stream_response, name="stream_response"),
     path("ver_fontes/<int:id>", views.ver_fontes, name="ver_fontes"),
     path("webhook_whatsapp/", views.webhook_whatsapp, name="webhook_whatsapp"),
+    path("webhook_whatsapp", views.webhook_whatsapp),
 ]
